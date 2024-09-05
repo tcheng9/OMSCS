9ec755d598984995ad36570cbecf59dcac3c5631
tommy <tcheng789@gmail.com>
2024-09-05 18:05:39 -0500

    working on optimize something code. I've translated calculating sharpe ratio over. Now I just need to figure out how to run optimize something

--- ML4T/optimize_something/optimization.py
+++ ML4T/optimize_something/optimization.py
@@ -34,7 +34,7 @@ import numpy as np
 import matplotlib.pyplot as plt  		  	   		 	   		  		  		    	 		 		   		 		  
 import pandas as pd  		  	   		 	   		  		  		    	 		 		   		 		  
 from util import get_data, plot_data
-
+import math
 
 def author():
     """
@@ -47,6 +47,60 @@ def author():
 def study_group():
     return "tcheng99"
 
+def calc_sr(
+    sd,
+    ed,
+    syms,
+    allocs,
+    sv=1000000,
+    rfr=0.0,
+    sf=252.0,
+):
+    #STEP 1: GETTING ALL DATA
+    # Read in adjusted closing prices for given symbols, date range
+    dates = pd.date_range(sd, ed)
+    prices_all = get_data(syms, dates)  # automatically adds SPY
+    prices = prices_all[syms]  # only portfolio symbols
+    prices_SPY = prices_all["SPY"]  # only SPY, for comparison later
+
+    '''
+    -----------CALCULATING DAILY PORTFOLIO VALUE----------
+    '''
+    # print(prices_all)
+    normed = prices / prices.iloc[0].values
+    alloced = normed * allocs
+    pos_vals = alloced * sv
+    port_val = pos_vals.sum(axis=1)  # daily portfolio value
+    cr = (port_val.iloc[-1] / port_val.iloc[0]) - 1
+
+    '''
+    ----------GETTING PORTFOLIO STATISTICS ------------------
+    '''
+    # daily returns if using port_val
+    daily_returns_all = port_val.copy()
+    daily_returns_all.iloc[1:] = (daily_returns_all.iloc[1:] / daily_returns_all.iloc[:-1].values) - 1
+    # daily_returns_all.iloc[0] = 0
+    daily_returns_all = daily_returns_all[1:]
+    # 4. mean
+    adr = daily_returns_all.mean()
+
+    # 5. standard deviation
+    sddr = daily_returns_all.std()
+
+    # 6. Sharpe Ratio
+    # sr = math.sqrt(sf)*(daily_returns_all.mean() - rfr/sddr)
+    sr = math.sqrt(sf) * (adr - rfr) / sddr
+    ev = port_val[-1]
+
+
+    print('cr is', cr)
+    print('adr is', adr)
+    print('sddr is', sddr)
+    print('sr is', sr)
+    print('ev is', ev)
+
+
+    return cr, adr, sddr, sr, ev
 
 # This is the function that will be tested by the autograder
 # The student must update this code to properly implement the functionality  		  	   		 	   		  		  		    	 		 		   		 		  
@@ -77,24 +131,31 @@ def optimize_portfolio(
         standard deviation of daily returns, and Sharpe ratio  		  	   		 	   		  		  		    	 		 		   		 		  
     :rtype: tuple  		  	   		 	   		  		  		    	 		 		   		 		  
     """  		  	   		 	   		  		  		    	 		 		   		 		  
-  		  	   		 	   		  		  		    	 		 		   		 		  
-    # Read in adjusted closing prices for given symbols, date range  		  	   		 	   		  		  		    	 		 		   		 		  
-    dates = pd.date_range(sd, ed)  		  	   		 	   		  		  		    	 		 		   		 		  
-    prices_all = get_data(syms, dates)  # automatically adds SPY  		  	   		 	   		  		  		    	 		 		   		 		  
-    prices = prices_all[syms]  # only portfolio symbols  		  	   		 	   		  		  		    	 		 		   		 		  
-    prices_SPY = prices_all["SPY"]  # only SPY, for comparison later  		  	   		 	   		  		  		    	 		 		   		 		  
-  		  	   		 	   		  		  		    	 		 		   		 		  
+    print('\n')
+    # Read in adjusted closing prices for given symbols, date range
+    dates = pd.date_range(sd, ed)
+    prices_all = get_data(syms, dates)  # automatically adds SPY
+    prices = prices_all[syms]  # only portfolio symbols
+    prices_SPY = prices_all["SPY"]  # only SPY, for comparison later
+
     # find the allocations for the optimal portfolio  		  	   		 	   		  		  		    	 		 		   		 		  
     # note that the values here ARE NOT meant to be correct for a test case  		  	   		 	   		  		  		    	 		 		   		 		  
     allocs = np.asarray(  		  	   		 	   		  		  		    	 		 		   		 		  
-        [0.2, 0.2, 0.3, 0.3]  		  	   		 	   		  		  		    	 		 		   		 		  
-    )  # add code here to find the allocations  		  	   		 	   		  		  		    	 		 		   		 		  
-    cr, adr, sddr, sr = [  		  	   		 	   		  		  		    	 		 		   		 		  
-        0.25,  		  	   		 	   		  		  		    	 		 		   		 		  
-        0.001,  		  	   		 	   		  		  		    	 		 		   		 		  
-        0.0005,  		  	   		 	   		  		  		    	 		 		   		 		  
-        2.1,  		  	   		 	   		  		  		    	 		 		   		 		  
-    ]  # add code here to compute stats  		  	   		 	   		  		  		    	 		 		   		 		  
+        [0.2, 0.2, 0.2,0.2,0.2,]
+    )  # add code here to find the allocations
+
+    '''
+    THIS IS WHERE YOU HAVE CODE FOR FINDING OPTIMAL SHARPE RATIO
+    '''
+    calc_sr(sd, ed, syms, allocs, 1000000, 0.0, 252.0)
+
+
+    cr, adr, sddr, sr = [
+        0.25,
+        0.001,
+        0.0005,
+        2.1,
+    ]  # add code here to compute stats
   		  	   		 	   		  		  		    	 		 		   		 		  
     # Get daily portfolio value  		  	   		 	   		  		  		    	 		 		   		 		  
     port_val = prices_SPY  # add code here to compute daily portfolio values  		  	   		 	   		  		  		    	 		 		   		 		  
@@ -125,17 +186,82 @@ def test_code():
     )  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
     # Print statistics  		  	   		 	   		  		  		    	 		 		   		 		  
-    print(f"Start Date: {start_date}")  		  	   		 	   		  		  		    	 		 		   		 		  
-    print(f"End Date: {end_date}")  		  	   		 	   		  		  		    	 		 		   		 		  
-    print(f"Symbols: {symbols}")  		  	   		 	   		  		  		    	 		 		   		 		  
-    print(f"Allocations:{allocations}")  		  	   		 	   		  		  		    	 		 		   		 		  
-    print(f"Sharpe Ratio: {sr}")  		  	   		 	   		  		  		    	 		 		   		 		  
-    print(f"Volatility (stdev of daily returns): {sddr}")  		  	   		 	   		  		  		    	 		 		   		 		  
-    print(f"Average Daily Return: {adr}")  		  	   		 	   		  		  		    	 		 		   		 		  
-    print(f"Cumulative Return: {cr}")  		  	   		 	   		  		  		    	 		 		   		 		  
+    # print(f"Start Date: {start_date}")
+    # print(f"End Date: {end_date}")
+    # print(f"Symbols: {symbols}")
+    # print(f"Allocations:{allocations}")
+    # print(f"Sharpe Ratio: {sr}")
+    # print(f"Volatility (stdev of daily returns): {sddr}")
+    # print(f"Average Daily Return: {adr}")
+    # print(f"Cumulative Return: {cr}")
   		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
 if __name__ == "__main__":  		  	   		 	   		  		  		    	 		 		   		 		  
     # This code WILL NOT be called by the auto grader  		  	   		 	   		  		  		    	 		 		   		 		  
     # Do not assume that it will be called  		  	   		 	   		  		  		    	 		 		   		 		  
     test_code()  		  	   		 	   		  		  		    	 		 		   		 		  
+
+
+
+'''
+original (incorrect optimize_portfolio fn)
+def optimize_portfolio(  		  	   		 	   		  		  		    	 		 		   		 		  
+    sd=dt.datetime(2008, 1, 1),  		  	   		 	   		  		  		    	 		 		   		 		  
+    ed=dt.datetime(2009, 1, 1),  		  	   		 	   		  		  		    	 		 		   		 		  
+    syms=["GOOG", "AAPL", "GLD", "XOM"],  		  	   		 	   		  		  		    	 		 		   		 		  
+    gen_plot=False,  		  	   		 	   		  		  		    	 		 		   		 		  
+):  		  	   		 	   		  		  		    	 		 		   		 		  
+    """  		  	   		 	   		  		  		    	 		 		   		 		  
+    This function should find the optimal allocations for a given set of stocks. You should optimize for maximum Sharpe  		  	   		 	   		  		  		    	 		 		   		 		  
+    Ratio. The function should accept as input a list of symbols as well as start and end dates and return a list of  		  	   		 	   		  		  		    	 		 		   		 		  
+    floats (as a one-dimensional numpy array) that represents the allocations to each of the equities. You can take  		  	   		 	   		  		  		    	 		 		   		 		  
+    advantage of routines developed in the optional assess portfolio project to compute daily portfolio value and  		  	   		 	   		  		  		    	 		 		   		 		  
+    statistics.  		  	   		 	   		  		  		    	 		 		   		 		  
+  		  	   		 	   		  		  		    	 		 		   		 		  
+    :param sd: A datetime object that represents the start date, defaults to 1/1/2008  		  	   		 	   		  		  		    	 		 		   		 		  
+    :type sd: datetime  		  	   		 	   		  		  		    	 		 		   		 		  
+    :param ed: A datetime object that represents the end date, defaults to 1/1/2009  		  	   		 	   		  		  		    	 		 		   		 		  
+    :type ed: datetime  		  	   		 	   		  		  		    	 		 		   		 		  
+    :param syms: A list of symbols that make up the portfolio (note that your code should support any  		  	   		 	   		  		  		    	 		 		   		 		  
+        symbol in the data directory)  		  	   		 	   		  		  		    	 		 		   		 		  
+    :type syms: list  		  	   		 	   		  		  		    	 		 		   		 		  
+    :param gen_plot: If True, optionally create a plot named plot.png. The autograder will always call your  		  	   		 	   		  		  		    	 		 		   		 		  
+        code with gen_plot = False.  		  	   		 	   		  		  		    	 		 		   		 		  
+    :type gen_plot: bool  		  	   		 	   		  		  		    	 		 		   		 		  
+    :return: A tuple containing the portfolio allocations, cumulative return, average daily returns,  		  	   		 	   		  		  		    	 		 		   		 		  
+        standard deviation of daily returns, and Sharpe ratio  		  	   		 	   		  		  		    	 		 		   		 		  
+    :rtype: tuple  		  	   		 	   		  		  		    	 		 		   		 		  
+    """  		  	   		 	   		  		  		    	 		 		   		 		  
+    
+    # Read in adjusted closing prices for given symbols, date range  		  	   		 	   		  		  		    	 		 		   		 		  
+    dates = pd.date_range(sd, ed)  		  	   		 	   		  		  		    	 		 		   		 		  
+    prices_all = get_data(syms, dates)  # automatically adds SPY
+    prices = prices_all[syms]  # only portfolio symbols  		  	   		 	   		  		  		    	 		 		   		 		  
+    prices_SPY = prices_all["SPY"]  # only SPY, for comparison later  		  	   		 	   		  		  		    	 		 		   		 		  
+  		  	   		 	   		  		  		    	 		 		   		 		  
+    # find the allocations for the optimal portfolio  		  	   		 	   		  		  		    	 		 		   		 		  
+    # note that the values here ARE NOT meant to be correct for a test case  		  	   		 	   		  		  		    	 		 		   		 		  
+    allocs = np.asarray(  		  	   		 	   		  		  		    	 		 		   		 		  
+        [0.2, 0.2, 0.3, 0.3]  		  	   		 	   		  		  		    	 		 		   		 		  
+    )  # add code here to find the allocations  		  	   		 	   		  		  		    	 		 		   		 		  
+    cr, adr, sddr, sr = [  		  	   		 	   		  		  		    	 		 		   		 		  
+        0.25,  		  	   		 	   		  		  		    	 		 		   		 		  
+        0.001,  		  	   		 	   		  		  		    	 		 		   		 		  
+        0.0005,  		  	   		 	   		  		  		    	 		 		   		 		  
+        2.1,  		  	   		 	   		  		  		    	 		 		   		 		  
+    ]  # add code here to compute stats  		  	   		 	   		  		  		    	 		 		   		 		  
+  		  	   		 	   		  		  		    	 		 		   		 		  
+    # Get daily portfolio value  		  	   		 	   		  		  		    	 		 		   		 		  
+    port_val = prices_SPY  # add code here to compute daily portfolio values  		  	   		 	   		  		  		    	 		 		   		 		  
+  		  	   		 	   		  		  		    	 		 		   		 		  
+    # Compare daily portfolio value with SPY using a normalized plot  		  	   		 	   		  		  		    	 		 		   		 		  
+    if gen_plot:  		  	   		 	   		  		  		    	 		 		   		 		  
+        # add code to plot here  		  	   		 	   		  		  		    	 		 		   		 		  
+        df_temp = pd.concat(  		  	   		 	   		  		  		    	 		 		   		 		  
+            [port_val, prices_SPY], keys=["Portfolio", "SPY"], axis=1  		  	   		 	   		  		  		    	 		 		   		 		  
+        )  		  	   		 	   		  		  		    	 		 		   		 		  
+        pass  		  	   		 	   		  		  		    	 		 		   		 		  
+  		  	   		 	   		  		  		    	 		 		   		 		  
+    return allocs, cr, adr, sddr, sr 
+
+'''
