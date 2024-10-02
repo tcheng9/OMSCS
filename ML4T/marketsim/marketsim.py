f5d28b37819750628b5f98f4357827417ec42970
tommy <tcheng789@gmail.com>
2024-10-02 13:00:02 -0500

    submitting proj 5

--- ML4T/marketsim/marketsim.py
+++ ML4T/marketsim/marketsim.py
@@ -23,7 +23,7 @@ GT honor code violation.
   		  	   		 	   		  		  		    	 		 		   		 		  
 Student Name: Tommy Cheng (replace with your name)  		  	   		 	   		  		  		    	 		 		   		 		  
 GT User ID: tcheng99 (replace with your User ID)  		  	   		 	   		  		  		    	 		 		   		 		  
-GT ID:  (replace with your GT ID)  		  	   		 	   		  		  		    	 		 		   		 		  
+GT ID: 903967530  (replace with your GT ID)  		  	   		 	   		  		  		    	 		 		   		 		  
 """  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
 import datetime as dt  		  	   		 	   		  		  		    	 		 		   		 		  
@@ -33,22 +33,22 @@ import numpy as np
   		  	   		 	   		  		  		    	 		 		   		 		  
 import pandas as pd  		  	   		 	   		  		  		    	 		 		   		 		  
 from util import get_data, plot_data  		  	   		 	   		  		  		    	 		 		   		 		  
-import optimize_something.optimization as opt
-pd.set_option('display.max_columns', None)
-pd.set_option("display.max_rows", None)
-pd.options.display.float_format = '{:.2f}'.format
+# import optimize_something.optimization as opt
+# pd.set_option('display.max_columns', None)
+# pd.set_option("display.max_rows", None)
+# pd.options.display.float_format = '{:.2f}'.format
 def build_prices(df):
-    print('\n')
+
     start_date = min(df['Date'])
     end_date = max(df['Date'])
-    # print(start_date, '|', end_date)
+
     stocks = df['Symbol'].unique()
     # start_date = dt.datetime(2011, 1, 1)
     # end_date = dt.datetime(2011, 12, 31)
     prices = get_data(stocks, pd.date_range(start_date, end_date))
     prices = prices[stocks]  # remove SPY
     prices['Cash'] = 1.00
-    # print(prices)
+
     return prices
 
 def build_trades(csv_df, prices, commission, impact):
@@ -58,7 +58,6 @@ def build_trades(csv_df, prices, commission, impact):
     # trades = pd.DataFrame(index = csv_df.index, columns = stocks)
     # trades['Cash'] = 1
     # trades.fillna(0)
-    # print(trades)
 
     # approach 2: copy an old one and clean it
     trades = prices.copy(deep=True)
@@ -69,13 +68,9 @@ def build_trades(csv_df, prices, commission, impact):
     for i in range(csv_df.shape[0]):
 
         date, symbol, order, shares = csv_df.iloc[i, :]
-        # print(date)
-        # print(symbol)
-        # print(order)
-        # print(shares)
-        # print(date, symbol, order, shares)
+
         price = prices.loc[date, symbol]
-        # print('price of', symbol, 'is', price)
+
         if order == "BUY":
             #if you buy, cash goes down but shares go up
             trades.loc[date, 'Cash'] = trades.loc[date, 'Cash'] + (-1 * ((( price + (impact * price)) * shares)+commission)   )
@@ -88,7 +83,7 @@ def build_trades(csv_df, prices, commission, impact):
             trades.loc[date, symbol] = trades.loc[date, symbol] + (-1*shares)
 
     ##creating an empty trades DF to add info to
-    # print(trades)
+
     return trades
 
 def build_holdings(prices, trades, start_val):
@@ -100,15 +95,12 @@ def build_holdings(prices, trades, start_val):
 
     # holdings.loc[:, 'AAPL'] = trades.loc[:, 'AAPL']
 
-    # print(rows, cols)
 
     #day 0 case:
     holdings.iloc[0, -1] = start_val
 
     holdings = holdings.cumsum(axis =0) + trades.cumsum(axis=0)
-    # print('holdings 2')
-    # print(holdings)
-    # print('---------')
+
     #day 1 - end case;
 
 
@@ -128,9 +120,7 @@ def build_values(prices, holdings):
         for c in range(cols):
             values.iloc[r, c] = holdings.iloc[r, c] * prices.iloc[r, c]
 
-    # print('values table')
-    # print(values)
-    # print('------------------------------------------')
+
     return values
 
 def build_daytotal(values):
@@ -140,9 +130,7 @@ def build_daytotal(values):
     day_total = values.sum(axis = 1)
 
 
-    # print('day total table')
-    # print(day_total)
-    # print('------------------------------------------')
+
     return day_total
 
 
@@ -172,11 +160,11 @@ def compute_portvals(
 
 
     # TODO: Your code here
-    print('\n')
+
     # df = pd.read_csv('./orders/orders-01.csv',  parse_dates=True, na_values = ['nan'])
     df = pd.read_csv(orders_file, parse_dates=True, na_values = ['nan'])
     df = df.sort_values(by = "Date")
-    # print(df)
+
 
     prices = build_prices(df)
     trades = build_trades(df, prices, commission, impact)
@@ -184,64 +172,63 @@ def compute_portvals(
     values = build_values(prices, holdings)
 
     day_total = build_daytotal(values)
-    # print(day_total)
-    # print('end of my code')
+
     return day_total
 
 
 
-def test_code():  		  	   		 	   		  		  		    	 		 		   		 		  
-    """  		  	   		 	   		  		  		    	 		 		   		 		  
-    Helper function to test code  		  	   		 	   		  		  		    	 		 		   		 		  
-    """  		  	   		 	   		  		  		    	 		 		   		 		  
-    # this is a helper function you can use to test your code  		  	   		 	   		  		  		    	 		 		   		 		  
-    # note that during autograding his function will not be called.  		  	   		 	   		  		  		    	 		 		   		 		  
-    # Define input parameters  		  	   		 	   		  		  		    	 		 		   		 		  
-  		  	   		 	   		  		  		    	 		 		   		 		  
-    of = "./orders/orders-01.csv"
-    sv = 1000000  		  	   		 	   		  		  		    	 		 		   		 		  
-    df = pd.read_csv(of)
-    print('\n')
-    # print(df)
-    # Process orders  		  	   		 	   		  		  		    	 		 		   		 		  
-    portvals = compute_portvals(orders_file=of, start_val=sv)  		  	   		 	   		  		  		    	 		 		   		 		  
-    if isinstance(portvals, pd.DataFrame):  		  	   		 	   		  		  		    	 		 		   		 		  
-        portvals = portvals[portvals.columns[0]]  # just get the first column  		  	   		 	   		  		  		    	 		 		   		 		  
-    else:  		  	   		 	   		  		  		    	 		 		   		 		  
-        "warning, code did not return a DataFrame"
-    # print(portvals)
-    # Get portfolio stats  		  	   		 	   		  		  		    	 		 		   		 		  
-    # Here we just fake the data. you should use your code from previous assignments.  		  	   		 	   		  		  		    	 		 		   		 		  
-    start_date = min(df['Date'])
-    end_date = max(df['Date'])
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio, ev, portvals =opt.assess_portfolio(
-        start_date,
-        end_date,
-        syms = df['Symbol'].unique(),
-        allocs = [.2, .2, .2, .2],
-        sv = 1000000,
-        rfr = 0.0,
-        sf = 252.0
-    )
-
-    # print(cum_ret)
-
-    # Compare portfolio against $SPX
-    # print(f"Date Range: {start_date} to {end_date}")
-    # print()
-    # print(f"Sharpe Ratio of Fund: {sharpe_ratio}")
-    # # print(f"Sharpe Ratio of SPY : {sharpe_ratio_SPY}")
-    # print()
-    # print(f"Cumulative Return of Fund: {cum_ret}")
-    # # print(f"Cumulative Return of SPY : {cum_ret_SPY}")
-    # print()
-    # print(f"Standard Deviation of Fund: {std_daily_ret}")
-    # # print(f"Standard Deviation of SPY : {std_daily_ret_SPY}")
-    # print()
-    # print(f"Average Daily Return of Fund: {avg_daily_ret}")
-    # # print(f"Average Daily Return of SPY : {avg_daily_ret_SPY}")
-    # print()
-    # print(f"Final Portfolio Value: {portvals[-1]}")
+# def test_code():
+#     """
+#     Helper function to test code
+#     """
+#     # this is a helper function you can use to test your code
+#     # note that during autograding his function will not be called.
+#     # Define input parameters
+#
+#     of = "./orders/orders-02.csv"
+#     sv = 1000000
+#     df = pd.read_csv(of)
+#
+#     # Process orders
+#     portvals = compute_portvals(orders_file=of, start_val=sv)
+#     if isinstance(portvals, pd.DataFrame):
+#         portvals = portvals[portvals.columns[0]]  # just get the first column
+#     else:
+#         "warning, code did not return a DataFrame"
+#
+#     # Get portfolio stats
+#     # Here we just fake the data. you should use your code from previous assignments.
+#     start_date = min(df['Date'])
+#     end_date = max(df['Date'])
+#     allocs = [1 / len(df['Symbol'].unique())] * len(df['Symbol'].unique())
+#     cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio, ev, portvals =opt.assess_portfolio(
+#         start_date,
+#         end_date,
+#         syms = df['Symbol'].unique(),
+#         allocs = allocs,
+#         sv = 1000000,
+#         rfr = 0.0,
+#         sf = 252.0
+#     )
+#
+#
+#
+#     # # Compare portfolio against $SPX
+#     print(f"Date Range: {start_date} to {end_date}")
+#     print()
+#     print(f"Sharpe Ratio of Fund: {sharpe_ratio}")
+#     # print(f"Sharpe Ratio of SPY : {sharpe_ratio_SPY}")
+#     print()
+#     print(f"Cumulative Return of Fund: {cum_ret}")
+#     # print(f"Cumulative Return of SPY : {cum_ret_SPY}")
+#     print()
+#     print(f"Standard Deviation of Fund: {std_daily_ret}")
+#     # print(f"Standard Deviation of SPY : {std_daily_ret_SPY}")
+#     print()
+#     print(f"Average Daily Return of Fund: {avg_daily_ret}")
+#     # print(f"Average Daily Return of SPY : {avg_daily_ret_SPY}")
+#     print()
+#     print(f"Final Portfolio Value: {portvals[-1]}")
   		  	   		 	   		  		  		    	 		 		   		 		  
 def author():
     """  		  	   		 	   		  		  		    	 		 		   		 		  
@@ -254,29 +241,6 @@ def study_group(self):
 
     return 'tcheng99'
 
-if __name__ == "__main__":  		  	   		 	   		  		  		    	 		 		   		 		  
-    test_code()  		  	   		 	   		  		  		    	 		 		   		 		  
-
-
-'''
-# # In the template, instead of computing the value of the portfolio, we just
-    # # read in the value of IBM over 6 months
-    # start_date = dt.datetime(2008, 1, 1)
-    # end_date = dt.datetime(2008, 6, 1)
-    # portvals = get_data(["IBM", "AAPL"], pd.date_range(start_date, end_date))
-    # portvals = portvals[["IBM", "AAPL"]]  # remove SPY
-    # print('ibm')
-    # print(portvals)
-    # rv = pd.DataFrame(index=portvals.index, data=portvals.values)
-  	#
-    # return rv
-    # return portvals
-    # df = pd.read_csv('./orders/orders-01.csv')
-    # orders_df = pd.read_csv(orders_file, index_col=’Date’, parse_dates = True, na_values = [‘nan’])
-    # # print('\n', df.head())
-    # # df.head()
-    # return rv
-'''
-# Date,Symbol,Order,Shares
-
+# if __name__ == "__main__":
+#     test_code()
 
